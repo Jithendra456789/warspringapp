1ff42600a72681c3c404926737aedf81d942fe57
Jithendra456789 <jithendra456789@gmail.com>
2020-08-07 14:39:53 +0530

    my war app

---
+++ src/main/java/com/example/myspringwar/MyspringwarApplication.java
@@ -0,0 +1,13 @@
+package com.example.myspringwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringwarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringwarApplication.class, args);
+	}
+
+}
